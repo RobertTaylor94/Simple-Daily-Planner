8acdea40ce43230d954bde686a14076d434c499c
Robert Taylor <rjt903@gmail.com>
2023-01-20 16:18:22 +0000

    function to render 9 rows for each hour slot added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-var currentDay = moment().format("Do MMM, YYYY")
+var currentDay = moment().format("Do MMM, YYYY");
 
 //Document selectors
-var dayTitle = $("#currentDay")
+var dayTitle = $("#currentDay");
+var container = $(".container");
 
-dayTitle.text(currentDay)
+dayTitle.text(currentDay);
 
 function clearNewDay() {
     //if the stored date does not match todays date, clear the page
@@ -11,7 +12,30 @@ function clearNewDay() {
 
 function renderDay() {
     //create 9 blocks representing one hour each from 9am to 5pm
+
+    function renderRow() {
+        var newRow = $("<div>");
+        var timeDiv = $("<div>");
+        var saveButton = $("<button>");
+        var textArea = $("<textarea>");
+    
+        newRow.addClass("row col-12 ");
+        timeDiv.addClass("hour col-2");
+        textArea.addClass("description col-9 past");
+        saveButton.addClass("saveBtn col-1 fas fa-save");
+    
+        newRow.append(timeDiv);
+        newRow.append(textArea);
+        newRow.append(saveButton);
+        container.append(newRow);
+    }
+
+    for (i = 0; i < 9; i++) {
+        renderRow()
+    };
 }
+
+renderDay();
 
 //colour code each block whether it is the past, present or future
 

94f70081ebc9825a6ebb2dc0e8f0f50217df56e5
Robert Taylor <rjt903@gmail.com>
2023-01-20 16:50:01 +0000

    Each row now rendered in HTML not JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var currentDay = moment().format("Do MMM, YYYY");
+var currentHour = moment().format("hA");
 
 //Document selectors
 var dayTitle = $("#currentDay");
@@ -11,31 +12,7 @@ function clearNewDay() {
 };
 
 function renderDay() {
-    //create 9 blocks representing one hour each from 9am to 5pm
 
-    function renderRow(hour) {
-        //creates all the HTML elements for each row
-        var newRow = $("<div>");
-        var timeDiv = $("<div>");
-        var saveButton = $("<button>");
-        var textArea = $("<textarea>");
-        
-        //adds classes to eahc HTMl element
-        newRow.addClass("row col-12 ");
-        timeDiv.addClass("hour col-2");
-        timeDiv.append(hour);
-        textArea.addClass("description col-9 past");
-        saveButton.addClass("saveBtn col-1 fas fa-save");
-    
-        //appends the HTML elements to the row
-        newRow.append(textArea);
-        newRow.append(timeDiv);
-        newRow.append(textArea);
-        newRow.append(saveButton);
-        container.append(newRow);
-    }
-
-    //creates a new row for each hour
     for (i = 0; i < 9; i++) {
         var hour = moment().hour(i+9).format("hA");
         firstHour = moment().add(1, "h")
@@ -43,15 +20,13 @@ function renderDay() {
     };
 };
 
-renderDay();
 
-// console.log(moment().hour().format("A"))
 
 //colour code each block whether it is the past, present or future
 
 
 //Allow a user to enter an event when they click on a time block
-//The event should save to local storage on a button click
+//The event should save to local storage on save button click
 
 
 //persist events between page refreshes

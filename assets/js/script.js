f6677a2dc9a3566b9b0ff09b927221f603e6ee49
Robert Taylor <rjt903@gmail.com>
2023-01-21 17:56:50 +0000

    each row is now coloured for past/present/future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var currentDay = moment().format("Do MMM, YYYY");
-var currentHour = moment().format("hA");
+var currentHour = moment().format("H");
 
 //Document selectors
 var dayTitle = $("#currentDay");
 var container = $(".container");
+var rows = $(".row");
 
 dayTitle.text(currentDay);
 
@@ -11,22 +12,30 @@ function clearNewDay() {
     //if the stored date does not match todays date, clear the page
 };
 
-function renderDay() {
+function setColor() {
+    //for each row in the day set the color based on its time slot
+    for (i = 0; i < rows.length; i++) {
+        var textArea = rows[i].children[1];
+        var rowTime = rows[i].children[0].dataset.hour;
 
-    for (i = 0; i < 9; i++) {
-        var hour = moment().hour(i+9).format("hA");
-        firstHour = moment().add(1, "h")
-        renderRow(hour);
-    };
-};
+        if (rowTime < currentHour) {
+            //if the time is less than the current hour its in the past and gray
+            textArea.setAttribute("class", "past description col-9");
+        } else if (rowTime === currentHour) {
+            //if the time matches the current hour its the present and the row is red
+            textArea.setAttribute("class", "present description col-9");
+        } else if (rowTime > currentHour) {
+            //if the time is greater than the current hour its in the future and green
+            textArea.setAttribute("class", "future description col-9");
+        };
+        };
+    }
 
-
-
-//colour code each block whether it is the past, present or future
-
+setColor();
 
 //Allow a user to enter an event when they click on a time block
 //The event should save to local storage on save button click
 
 
 //persist events between page refreshes
+

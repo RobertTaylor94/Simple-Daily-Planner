ba78b7c1cee42c93f88fa76ff9a5bf33b4bbf407
Robert Taylor <rjt903@gmail.com>
2023-01-21 20:56:44 +0000

    Added persistence between page refreshes using localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 var currentDay = moment().format("Do MMM, YYYY");
 var currentHour = moment().format("H");
+var schedule = [
+    {time: 09, task: ""},
+    {time: 10, task: ""},
+    {time: 11, task: ""},
+    {time: 12, task: ""},
+    {time: 13, task: ""},
+    {time: 14, task: ""},
+    {time: 15, task: ""},
+    {time: 16, task: ""},
+    {time: 17, task: ""},
+];
 
 //Document selectors
 var dayTitle = $("#currentDay");
@@ -36,6 +47,37 @@ setColor();
 //Allow a user to enter an event when they click on a time block
 //The event should save to local storage on save button click
 
+function saveEvent(event) {
+    var parent = $(event.target).parent()
+    var text = parent.children().eq(1).val();
+    var index = parent.children().eq(1)[0].dataset.index;
+
+    schedule[index].task = text;
+
+    var stringified = JSON.stringify(schedule);
+    localStorage.setItem("schedule", stringified);
+    console.log("saved")
+}
+
+rows.on("click", ".saveBtn", saveEvent)
 
 //persist events between page refreshes
+function loadEvents() {
+    var savedSchedule = JSON.parse(localStorage.getItem("schedule"));
 
+    if (savedSchedule === null) {
+        return;
+    }
+
+    schedule = savedSchedule;
+    console.log(schedule)
+    for (i = 0; i < 9; i++) {
+        var text = schedule[i].task;
+        rows[i].children[1].textContent = text;
+
+        console.log(text);
+    };
+
+};
+
+loadEvents();
